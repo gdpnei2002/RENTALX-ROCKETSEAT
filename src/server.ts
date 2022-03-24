98f3810b0cae716b5929c1dd150130181bfeca3c
neilson <joseneilson25@gmail.com>
2022-03-23 21:30:50 -0300

    aula 1

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,8 @@ import express from "express";
 
 const app = express();
 
-app.listen(3333, () => console.log("Server is"))
+app.get("/", (request, response) => {
+    return response.json({ message: "Hello World" })
+  });
+
+app.listen(3333, () => console.log("Server is ru"))
