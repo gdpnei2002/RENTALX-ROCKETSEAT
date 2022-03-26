fc688fa2f4a9f066178bdc8de67f13321a51e791
neilson <joseneilson25@gmail.com>
2022-03-25 23:42:24 -0300

    cadastro de categoria

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,10 @@
 import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello World" })
-  });
-
-  app.post("/courses", (request, response) => {
-  const { nane } = request.body;
-
-  return response.json({ nane });
-  })
+app.use(categoriesRoutes)
 
 app.listen(3333, () => console.log("Server is ru"))
